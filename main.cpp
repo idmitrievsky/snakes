3f424247e32df6869c2b287554a655fc63604340
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-04-11 13:22:15 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  snakes
+//
+//  Created by Ivan Dmitrievsky on 11/04/14.
+//  Copyright (c) 2014 Ivan Dmitrievsky. All rights reserved.
+//
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
