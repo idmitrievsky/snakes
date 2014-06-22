9ceab63e3c623f7587dfb804b02a0f34b9e73e79
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-06-23 01:15:07 +0400

    moved json to repo

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include "snake.h"
 
 int main(int argc, const char* argv[]) {
-  std::string snake_config("/Users/ivan/.supp/code/snakes/snake.json");
+  std::string snake_config("/Users/ivan/.supp/code/snakes/snakes/snake.json");
 
   Snake snake(snake_config);
 
